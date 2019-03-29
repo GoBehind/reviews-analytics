2e073889599c1f31ef6617eee5080bd7736b26ae
KuanChih Wang <n789142536@hotmail.com>
2019-03-29 17:19:07 +0800

    add dict

--- read.py
+++ read.py
@@ -1,22 +1,55 @@
 data = []
-count = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
 
-print('檔案讀取完了, 總共有', len(data), '筆資料')
+print('總共有', len(data), '筆資料, 檔案分析中請稍後 :)')
 
-sum_len = 0
+wc = {}
 for d in data:
-	sum_len += len(d)
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
 
-print('每筆留言平均長度是: ', sum_len / len(data))
+# for word in wc:
+# 	if wc[word] > 100:
+# 		print(word, '出現 ', wc[word], '次')
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆留言長度小於100')
+while True:
+	print('輸入[outofprogram]離開')
+	word = input('請輸入想查詢的字: ')
+	if word in wc:
+		print(word, '出現次數為: ', wc[word], '次')
+	
+	elif word == 'outofprogram':
+		break
+
+	else:
+		print('留言中沒有出現', word)
+		
+
+
+
+
+
+# sum_len = 0
+# for d in data:
+# 	sum_len += len(d)
+
+# print('每筆留言平均長度是: ', sum_len / len(data))
+
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+# print('一共有', len(new), '筆留言長度小於100')
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('一共有', len(good), '筆留言提到good')
